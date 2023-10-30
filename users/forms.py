9c1c4044e2508fe1e17127846617717892486909
eliseius <eliseevsi@gmail.com>
2023-10-31 00:18:06 +0300

    Corrected

--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(min_length=4, max_length=25)
-    email = forms.EmailField()
+    username = forms.CharField(min_length=4, max_length=25, unique=True)
+    email = forms.EmailField(unique=True)
     password = forms.CharField(min_length=8, max_length=40, widget=forms.PasswordInput)
     confirmpassword = forms.CharField(max_length=40, min_length=8, widget=forms.PasswordInput)
 
